3aa28ff5a18cf64e8f6b1788443117442238264d
Kevin Bronstein <bronsteinkevin@gmail.com>
2020-05-24 13:31:46 -0300

    Add todo handlers

---
+++ backend/src/handlers/todoHandlers.js
@@ -0,0 +1,26 @@
+const buildBaseHandlers = require('./baseHandlers')["buildHandlers"];
+
+const buildHandlers = ({ controller }) => {
+	if (!controller) {
+		throw new Error('The controller is required');
+    }
+
+    const getByPlace = (request, response, next) => {
+		try {
+			const result = controller.getByPlace({ placeId: request.params.placeId });
+			response.status(200).send(JSON.stringify(result));
+		}
+		catch (error) {
+			next(error);
+		}
+	};
+
+    return {
+        ...buildBaseHandlers({controller}),
+        getByPlace
+    }
+}
+
+module.exports = {
+	buildHandlers
+};
