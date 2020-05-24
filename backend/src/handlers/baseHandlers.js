cc90d3c6c28b150b4df8a12b2d339a3292a5e961
Kevin Bronstein <bronsteinkevin@gmail.com>
2020-05-24 13:31:38 -0300

    Add base handlers

---
+++ backend/src/handlers/baseHandlers.js
@@ -0,0 +1,70 @@
+const buildHandlers = ({ controller }) => {
+	if (!controller) {
+		throw new Error('The controller is required');
+	}
+
+	const getSingle = (request, response, next) => {
+		try {
+			const result = controller.getSingle({ id: request.params.id });
+			response.status(200).send(JSON.stringify(result));
+		}
+		catch (error) {
+			next(error);
+		}
+	};
+
+	const getAllPaginated = (request, response, next) => {
+		try {
+			const result = controller.getAllPaginated({ limit: request.params.limit, offset: request.params.offset });
+			response.status(200).send(JSON.stringify(result));
+		}
+		catch (error) {
+			next(error);
+		}
+	};
+
+	const create = (request, response, next) => {
+		try {
+			controller.create({ params: request.body });
+			response.status(201).send();
+		}
+		catch (error) {
+			next(error);
+		}
+	};
+
+	const update = (request, response, next) => {
+		try {
+			controller.update({ params: request.body });
+			response.status(204).send();
+		}
+		catch (error) {
+			next(error);
+		}
+	};
+
+	const delete = (request, response, next) => {
+		try {
+			controller.delete({ id: request.params.id });
+			response.status(204).send();
+		}
+		catch (error) {
+			next(error);
+		}
+	};
+
+
+	return {
+		getSingle,
+		getAllPaginated,
+		create,
+		update,
+		delete
+	};
+}
+
+
+
+module.exports = {
+	buildHandlers
+};
